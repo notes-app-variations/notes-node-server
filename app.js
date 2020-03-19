4b6dac115e0491e786a361d679b07bd9d5a766cf
Engedi <emese.engedi@accenture.com>
2020-03-19 13:36:07 +0100

    :art: listed endpoints

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from "express"
-import db from "./db/db"
 import bodyParser from "body-parser"
+import { connect } from "./connection.js"
+import cors from "cors"
+import {
+  getAllNotes,
+  getNote,
+  postNote,
+  editNote,
+  deleteNote
+} from "./db/noteActions.js"
+import { login, logout, register } from "./db/authActions.js"
 
 // Set up the express app
 const app = express()
@@ -8,43 +17,29 @@ const app = express()
 // Parse incoming requests data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
-// get all todos
-app.get("/api/v1/todos", (req, res) => {
-  res.status(200).send({
-    success: "true",
-    message: "todos retrieved successfully",
-    todos: db
-  })
-})
+// get all notes
+app.get("/api/notes", getAllNotes)
+app.get("/api/notes/:id", getNote)
+app.post("/api/notes", postNote)
+app.put("/api/notes/:id", editNote)
+app.delete("/api/notes/:id", deleteNote)
 
-app.post("/api/v1/todos", (req, res) => {
-  if (!req.body.title) {
-    return res.status(400).send({
-      success: "false",
-      message: "title is required"
-    })
-  } else if (!req.body.description) {
-    return res.status(400).send({
-      success: "false",
-      message: "description is required"
-    })
-  }
-  const todo = {
-    id: db.length + 1,
-    title: req.body.title,
-    description: req.body.description
-  }
-  db.push(todo)
-  return res.status(201).send({
-    success: "true",
-    message: "todo added successfully",
-    todo
-  })
-})
+app.post("/api/login", login)
+app.post("/api/logout", logout)
+app.post("/api/register", register)
 
 const PORT = 5000
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
-})
+const start = async () => {
+  try {
+    await connect()
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}/api`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
+start()
