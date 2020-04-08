def8d8ea803f7e3900d9aa7143ba23e59d03a366
Engedi <emese.engedi@accenture.com>
2020-04-08 13:58:10 +0200

    :bug: return user affter registering

--- db/authActions.js
+++ db/authActions.js
@@ -3,13 +3,13 @@ import { secretKey } from "../config.js"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-export const newToken = userId => {
+export const newToken = (userId) => {
   return jwt.sign({ id: userId }, secretKey, {
-    expiresIn: "2days"
+    expiresIn: "2days",
   })
 }
 
-export const verifyToken = token => {
+export const verifyToken = (token) => {
   try {
     return jwt.verify(token, secretKey)
   } catch (err) {
@@ -19,12 +19,9 @@ export const verifyToken = token => {
 
 exports.login = async (req, res) => {
   try {
-    const user = await client
-      .db("app_notes")
-      .collection("users")
-      .findOne({
-        email: req.body.email
-      })
+    const user = await client.db("app_notes").collection("users").findOne({
+      email: req.body.email,
+    })
     if (user) {
       console.log(
         `Found a user in the collection with the email '${req.body.email}'`
@@ -34,14 +31,14 @@ exports.login = async (req, res) => {
         const token = jwt.sign({ sub: user.id }, secretKey)
         return res.json({
           user: userWithoutHash,
-          token
+          token,
         })
       } else {
         return res.status(401).json({ error: "The password is not correct" })
       }
     } else
       return res.status(401).json({
-        error: `Wrong email: ${req.body.email}`
+        error: `Wrong email: ${req.body.email}`,
       })
   } catch (e) {
     console.error(`Couldn't get: ${e}`)
@@ -55,12 +52,9 @@ exports.register = async (req, res) => {
   if (user.email == "" || user.password == "" || user.username == "")
     return res.status(500).json({ error: "user info was not provided" })
   try {
-    await client
-      .db("app_notes")
-      .collection("users")
-      .findOne({
-        email: user.email
-      })
+    await client.db("app_notes").collection("users").findOne({
+      email: user.email,
+    })
   } catch (err) {
     return res
       .status(500)
@@ -78,7 +72,7 @@ exports.register = async (req, res) => {
       .insertOne(user)
     console.log(`New user created with the following id: ${result.insertedId}`)
     console.log(result.ops)
-    const { hash, user: userWithoutHash } = result.ops
+    const { hash, ...userWithoutHash } = result.ops[0]
     const token = newToken(result.insertedId)
     return res.json({ user: userWithoutHash, token })
   } catch (e) {
